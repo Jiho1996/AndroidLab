ca5e4f722c76684d5822f8812c180474aa657e7a
jiho1996 <jiho.urssu@gmail.com>
2020-11-10 03:27:07 +0900

    Lab06 DBHelper, ReadDBActivity추가.

---
+++ lab06/src/main/java/org/techtown/lab06/ReadDBActivity.java
@@ -0,0 +1,14 @@
+package org.techtown.lab06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReadDBActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_db);
+    }
+}
